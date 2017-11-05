1f5bc921b840a0ba4a1053ebfc2a00d50c92cbb2
rushTENm <rushtenm@gmail.com>
2017-11-05 23:00:49 +0900

    자바 GUI 기초
    p.431

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new NullContainerEx();
+    }
+}
+
+class NullContainerEx extends JFrame{
+    public NullContainerEx() {
+        setTitle("Null Container Sample");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container c = getContentPane();
+        c.setLayout(null);
+
+        JLabel la = new JLabel("Hello, Press Button");
+        la.setLocation(130,50);
+        la.setSize(200,20);
+        c.add(la);
+
+        for (int i = 0; i < 10; i++) {
+            JButton b =new JButton(Integer.toString(i));
+            b.setLocation(i*15,i*15);
+            b.setSize(50,20);
+            c.add(b);
+        }
+
+        setSize(300,200);
+        setVisible(true);
     }
 }
