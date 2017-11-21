aad5dd1399b64e361f05ddea81b32c55b58c01ba
JongSangLee <33251662+JongSangLee@users.noreply.github.com>
2017-11-22 01:57:37 +0900

    OrderView Gui입니다

---
+++ src/MainFrame.java
@@ -0,0 +1,32 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+public class MainFrame extends JFrame {
+	private CardLayout cards = new CardLayout();
+	
+	public CardLayout getCardLayout(){
+		return cards;
+	}
+	
+	public MainFrame(){
+		setSize(800, 600);
+		getContentPane().setLayout(cards);
+		setResizable(false);
+		
+		
+		
+		getContentPane().add("One", new P_One(this));
+		getContentPane().add("Two", new OrderView(this));
+		getContentPane().add("Three", new PaymentView(this)); 
+		
+		setVisible(true);
+	}
+	public void changePanel(){
+		cards.next(this.getContentPane());
+	}
+	public static void main(String[] args){
+		new  MainFrame();
+	}
+}
---
+++ src/P_One.java
@@ -0,0 +1,31 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+public class P_One extends JPanel {
+	private JLabel lblNewLabel;
+	private JButton btnNewButton1;
+	private JButton btnNewButton2;
+	private MainFrame F;
+	
+	public P_One(MainFrame f){
+		setBackground(Color.WHITE);
+		setSize(300, 80);
+		setLayout(null);
+		
+		F=f;
+		
+        btnNewButton1 = new JButton("주문/결제");
+        btnNewButton1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                F.changePanel();
+            }
+        });
+        btnNewButton1.setBounds(300, 250, 151, 62);
+        add(btnNewButton1);
+        
+        setVisible(true);
+	}
+
+}
